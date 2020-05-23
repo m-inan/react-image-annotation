9c41665ad815accb30d048e7174081721b8eaf7d
minan <muhammet@tilpark.com>
2020-05-23 04:55:32 +0300

    build settings

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+/* export default class Annotion extends React.Component { */
+/*   render() { */
+/*     return <div style={{ color: "red" }}>Hello</div>; */
+/*   } */
+/* } */
+
+export default function() {
+  return <div>hello</div>;
+}
+
